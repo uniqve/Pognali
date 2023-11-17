9ca56dea65b95f50a07735d1732aa13489ef7264
uniqve <vpluse98@mail.ru>
2023-11-17 19:01:06 +0300

    adaptive tablet and mobile

--- script/app.js
+++ script/app.js
@@ -1,7 +1,55 @@
-document.querySelector('.tariffs-buisnes').addEventListener('click', function() {
-    document.getElementById('modal').classList.add('active');
+document
+  .querySelector(".tariffs-buisnes")
+  .addEventListener("click", function () {
+    document.getElementById("modal").classList.add("active");
   });
+
+document.getElementById("closeModal").addEventListener("click", function () {
+  document.getElementById("modal").classList.remove("active");
+});
+
+const burgerButton = document.querySelector(".burger");
+const modal = document.querySelector(".burger__tablet-active");
+const closeButton = document.querySelector(".burger-close");
+const header = document.querySelector(".header");
+
+function openModal() {
+  modal.style.display = "block";
+}
+burgerButton.addEventListener("click", openModal);
+
+function closeModal() {
+  modal.style.display = "none";
+}
+closeButton.addEventListener("click", closeModal);
+
+burgerButton.addEventListener("click", () => {
+  header.style.display = "none";
+});
+
+closeButton.addEventListener("click", () => {
+  header.style.display = "flex";
+});
+
+
+const intrestedBlock = document.querySelector(".intrested");
+const intrestedButton = document.querySelector(".intrested-button");
+const footerBlock = document.querySelector('.footer')
+
+intrestedButton.addEventListener("click", () => {
+  intrestedBlock.classList.toggle("none");
+});
+
+
+
+
+window.addEventListener('scroll', function() {
+    var header = document.querySelector('.sticky-header');
+    var scrollPosition = window.scrollY;
   
-  document.getElementById('closeModal').addEventListener('click', function() {
-    document.getElementById('modal').classList.remove('active');
+    if (scrollPosition > 0) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
   });
